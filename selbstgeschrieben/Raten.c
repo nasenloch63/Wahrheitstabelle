c34202f8cb3cc43d034b59a5ce11b2df071fa3ea
nasenloch63 <yasin.aissani@gmx.de>
2026-07-23 09:10:58 +0200

    eingabe

--- selbstgeschrieben/Raten.c
+++ selbstgeschrieben/Raten.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
  int main() {
     int geheim = 42; 
-    int &eingabe; 
+    int eingabe; 
         printf("Rate die geheime Zahl: ");
     scanf("%d", &eingabe);
  return 0;   
