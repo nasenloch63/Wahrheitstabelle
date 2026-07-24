fdb7dc0f30c238e76bc0262e9b5fedfb0995ee6d
nasenloch63 <yasin.aissani@gmx.de>
2026-07-24 12:34:31 +0200

    hello von zuhause

--- hellozuhause.c
+++ hellozuhause.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
-int main() {
-    int geheim = 42; 
-    int eingabe; 
-    printf("Rate die geheime Zahl: ");
-    scanf("%d", &eingabe);
-    if (eingabe == geheim) {
-        printf("Richtig! Die geheime Zahl ist %d.\n", geheim);
-    } else {
-        printf("Falsch! Versuche es noch einmal.\n");
-    }
+int main() { 
+    printf("Halllo aus der WSL-Welt!\n");
+    
     return 0;   
 }
